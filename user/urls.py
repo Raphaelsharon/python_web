e51f27508a552c7c3d93f1c02290c932e00e078a
Raphaelsharon <santosraphael5766@gmail.com>
2023-09-20 20:07:31 -0300

    frash

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserViewPrivate,CreateUserView,CreateUserView,CustomTokenobtainPairView,LogoutView,AdminView
 
 # https://github.com/rg3915/gallery/blob/master/gallery
@@ -9,6 +10,7 @@ urlpatterns = [
      
      path('', csrf_exempt(CreateUserView.as_view())),
      path('token/', CustomTokenobtainPairView.as_view()),
+     path('token/refresh/', TokenRefreshView.as_view()),
      path('edite/', UserViewPrivate.as_view()),
      path("logout/", LogoutView.as_view(), name="logout"),
      path("admin/", AdminView.as_view(), name= "user-list"),
